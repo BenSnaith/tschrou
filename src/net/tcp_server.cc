cc66f5d655ba3ebb6b98985565ab280bda958fde
BenSnaith <bsnaith.bs@icloud.com>
2026-01-03 23:48:22 +0000

    feat: message.cc/h

--- src/net/tcp_server.cc
+++ src/net/tcp_server.cc
@@ -1,5 +1,13 @@
 #include "net/tcp_server.h"
 
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <sys/socket.h>
+
+#include <cstring>
+#include <iostream>
 
 namespace tsc::tcp {
 TcpServer::TcpServer(u16 port, Node* node) : port_(port), node_(node) {}
@@ -7,7 +15,117 @@ TcpServer::TcpServer(u16 port, Node* node) : port_(port), node_(node) {}
 TcpServer::~TcpServer() { Stop(); }
 
 bool TcpServer::Start() {
-  server_socket_ = socket()
+  server_socket_ = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket_ < 0) {
+    std::cerr << "Failed to create socket" << '\n';
+    return false;
+  }
+
+  int opt = 1;
+  setsockopt(server_socket_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+  sockaddr_in addr{};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(port_);
+
+  if (bind(server_socket_, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) <
+      0) {
+    std::cerr << "Failed to bind to port " << port_ << '\n';
+    close(server_socket_);
+    return false;
+  }
+
+  if (listen(server_socket_, 10) < 0) {
+    std::cerr << "Failed to listen on port " << port_ << '\n';
+    close(server_socket_);
+    return false;
+  }
+
+  int flags = fcntl(server_socket_, F_GETFL, 0);
+  fcntl(server_socket_, F_SETFL, flags | O_NONBLOCK);
+
+  running_ = true;
+  server_thread_ = std::jthread(&TcpServer::ServerLoop, this);
+
+  return true;
+}
+
+void TcpServer::Stop() {
+  running_ = false;
+
+  if (server_socket_ >= 0) {
+    close(server_socket_);
+    server_socket_ = -1;
+  }
+
+  if (server_thread_.joinable()) {
+    server_thread_.join();
+  }
+}
+
+void TcpServer::ServerLoop() {
+  while (running_) {
+    pollfd pfd{};
+    pfd.fd = server_socket_;
+    pfd.events = POLLIN;
+
+    int result = poll(&pfd, 1, 100);
+
+    if (result > 0 && (pfd.revents & POLLIN)) {
+      sockaddr_in client_addr{};
+      socklen_t addr_len = sizeof(client_addr);
+
+      int client_socket = accept(
+          server_socket_, reinterpret_cast<sockaddr*>(&client_addr), &addr_len);
+
+      if (client_socket >= 0) {
+        HandleClient(client_socket);
+      }
+    }
+  }
+}
+
+void TcpServer::HandleClient(int client_socket) {
+  timeval tv{};
+  tv.tv_sec = 5;
+  tv.tv_usec = 0;
+  setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+  std::vector<std::byte> buffer(4096);
+  ssize_t received = recv(client_socket, buffer.data(), buffer.size(), 0);
+
+  if (received > 0) {
+    buffer.resize(received);
+
+    auto response = ProcessMessage(buffer);
+
+    if (!response.empty()) {
+      send(client_socket, response.data(), response.size(), 0);
+    }
+  }
+
+  close(client_socket);
+}
+
+std::vector<std::byte> TcpServer::ProcessMessage(std::span<std::byte> message) {
+  if (message.empty()) {
+    return {};
+  }
+
+  MessageType type = GetMessageType(message);
+
+  try {
+    switch (type) {
+      case MessageType::FIND_SUCCESSOR_REQUEST: {
+
+      }
+
+      case
+    }
+  }
+
+  return {};
 }
 
 }  // namespace tsc::tcp
---
+++ src/protocol/message.h
@@ -0,0 +1,119 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <span>
+#include <vector>
+
+#include "types/types.h"
+
+using namespace tsc::type;
+
+namespace tsc::msg {
+enum class MessageType : u8 {
+  kFindSuccessorRequest = 0x01,
+  kFindSuccessorResponse = 0x02,
+  kGetPredecessorRequest = 0x03,
+  kGetPredecessorResponse = 0x04,
+  kNotify = 0x05,
+  kNotifyAck = 0x06,
+  kPing = 0x07,
+  kPong = 0x08,
+
+  kGetRequest = 0x10,
+  kGetResponse = 0x11,
+  kPutRequest = 0x12,
+  kPutResponse = 0x13,
+  kDeleteRequest = 0x14,
+  kDeleteResponse = 0x15,
+
+  kTransferKeysRequest = 0x20,
+  kTransferKeysResponse = 0x21,
+
+  kErrorResponse = 0xFF,
+};
+
+struct Message {
+  virtual std::vector<std::byte> Serialise() const = 0;
+
+  virtual ~Message() = default;
+
+  MessageType type_;
+};
+
+struct FindSuccessorRequest : Message {
+  FindSuccessorRequest() { type_ = MessageType::kFindSuccessorRequest; }
+  explicit FindSuccessorRequest(NodeID id) : id_(id) {
+    type_ = MessageType::kFindSuccessorRequest;
+  }
+
+  std::vector<std::byte> Serialise() const override;
+  static FindSuccessorRequest Deserialise(std::span<std::byte> data);
+
+  NodeID id_;
+};
+
+struct FindSuccessorResponse : Message {
+  FindSuccessorResponse() { type_ = MessageType::kFindSuccessorResponse; }
+
+  std::vector<std::byte> Serialise() const override;
+  static FindSuccessorResponse Deserialise(std::span<std::byte> data);
+
+  NodeInfo successor_;
+  bool found_;
+};
+
+struct GetPredecessorRequest : Message {
+  GetPredecessorRequest() { type_ = MessageType::kGetPredecessorRequest; }
+
+  std::vector<std::byte> Serialise() const override;
+  static GetPredecessorRequest Deserialise(std::span<std::byte> data);
+};
+
+struct GetPredecessorResponse : Message {
+  GetPredecessorResponse() { type_ = MessageType::kGetPredecessorResponse; }
+
+  std::vector<std::byte> Serialise() const override;
+  static GetPredecessorResponse Deserialise(std::span<std::byte> data);
+
+  NodeInfo predecessor_;
+  bool has_predecessor_;
+};
+
+struct NotifyMessage : Message {
+  NotifyMessage() { type_ = MessageType::kNotify; }
+  explicit NotifyMessage(const NodeInfo& node) : node_(node) {
+    type_ = MessageType::kNotify;
+  }
+
+  std::vector<std::byte> Serialise() const override;
+  static NotifyMessage Deserialise(std::span<std::byte> data);
+
+  NodeInfo node_;
+};
+
+struct NotifyAck : Message {
+  NotifyAck() { type_ = MessageType::kNotifyAck; }
+
+  std::vector<std::byte> Serialise() const override;
+  static NotifyAck Deserialise(std::span<std::byte> data);
+
+  bool accepted_;
+};
+
+struct PingMessage : Message {
+  PingMessage() { type_ = MessageType::kPing; }
+
+  std::vector<std::byte> Serialise() const override;
+  static PingMessage Deserialise(std::span<std::byte> data);
+};
+
+struct PongMessage : Message {
+  PongMessage() { type_ = MessageType::kPong; }
+
+  std::vector<std::byte> Serialise() const override;
+  static PongMessage Deserialise(std::span<std::byte> data);
+};
+
+} // namespace tsc::msg
+
+#endif // MESSAGE_H
